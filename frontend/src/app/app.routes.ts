6dd4f9dff9c7f1d366b11cd6f25ccf216bc9074b
Alessandra <alessandra.trotta2003@gmail.com>
2025-06-11 19:01:57 +0200

    modifica dei file app e del file index

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -18,4 +18,8 @@ export const routes: Routes = [
     path: 'home-benvenuto',
     loadComponent: () => import('./pages/home-benvenuto/home-benvenuto.page').then( m => m.HomeBenvenutoPage)
   },
+  {
+    path: 'registrazione',
+    loadComponent: () => import('./pages/registrazione/registrazione.page').then( m => m.RegistrazionePage)
+  },
 ];
