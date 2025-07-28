38f1ae914a1f0a7ee561f6fa868031ef3528bbcb
Alessandra <alessandra.trotta2003@gmail.com>
2025-07-28 17:19:35 +0200

    Modifica di area benessere, home-page e footer

--- frontend/src/app/pages/area-benessere/area-benessere.page.ts
+++ frontend/src/app/pages/area-benessere/area-benessere.page.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {
-
-
-} from '@ionic/angular/standalone';
-
-import { FooterComponent } from '../../component/footer/footer.component';
-import { RouterModule, Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, Location} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {} from '@ionic/angular/standalone';
+import {FooterComponent} from '../../component/footer/footer.component';
+import {RouterModule, Router} from '@angular/router';
 import {IonicModule} from "@ionic/angular";
 
 @Component({
-  selector: 'app-area-benessere',
-  templateUrl: './area-benessere.page.html',
-  styleUrls: ['./area-benessere.page.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule,
-    FooterComponent,
-    IonicModule
-  ]
+    selector: 'app-area-benessere',
+    templateUrl: './area-benessere.page.html',
+    styleUrls: ['./area-benessere.page.scss'],
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule,
+        FooterComponent,
+        IonicModule,
+    ]
 })
 export class AreaBenesserePage implements OnInit {
 
-  constructor(private router: Router) {}
+    constructor(private router: Router, private location: Location) {
+    }
 
-  ngOnInit(): void {}
+    ngOnInit(): void {
+    }
 
-  vaiA(path: string) {
-    this.router.navigate([path]);
-  }
+    vaiA(path: string) {
+        this.router.navigate([path]);
+    }
+
+    goBack() {
+        this.location.back();
+    }
 }
